255e7284ff383cd01c9952ac4df008323c307585
lushuyu <1053024839@qq.com>
2023-12-30 02:32:08 +0800

    complete lab2-3

--- kernel/strap.c
+++ kernel/strap.c
@@ -51,15 +51,16 @@ void handle_mtimer_trap() {
 //
 void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
     sprint("handle_page_fault: %lx\n", stval);
+    void *pag;
     switch (mcause) {
     case CAUSE_STORE_PAGE_FAULT:
         // TODO (lab2_3): implement the operations that solve the page fault to
         // dynamically increase application stack.
         // hint: first allocate a new physical page, and then, maps the new page
         // to the virtual address that causes the page fault.
-        panic("You need to implement the operations that actually handle the "
-              "page fault in lab2_3.\n");
-
+        pag = alloc_page();
+        user_vm_map(current->pagetable, stval / (PGSIZE) * (PGSIZE), PGSIZE,
+                    (uint64)(pag), prot_to_type(PROT_WRITE | PROT_READ, 1));
         break;
     default:
         sprint("unknown page fault.\n");
