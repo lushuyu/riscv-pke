eaf949a01ac7f08d070cf0f31de590b2ea33ae44
lushuyu <1053024839@qq.com>
2023-12-30 02:03:32 +0800

    complete lab1-2

--- kernel/machine/mtrap.c
+++ kernel/machine/mtrap.c
@@ -1,8 +1,10 @@
-#include "kernel/riscv.h"
 #include "kernel/process.h"
+#include "kernel/riscv.h"
 #include "spike_interface/spike_utils.h"
 
-static void handle_instruction_access_fault() { panic("Instruction access fault!"); }
+static void handle_instruction_access_fault() {
+    panic("Instruction access fault!");
+}
 
 static void handle_load_access_fault() { panic("Load access fault!"); }
 
@@ -15,36 +17,37 @@ static void handle_misaligned_load() { panic("Misaligned Load!"); }
 static void handle_misaligned_store() { panic("Misaligned AMO!"); }
 
 //
-// handle_mtrap calls a handling function according to the type of a machine mode interrupt (trap).
+// handle_mtrap calls a handling function according to the type of a machine
+// mode interrupt (trap).
 //
 void handle_mtrap() {
-  uint64 mcause = read_csr(mcause);
-  switch (mcause) {
+    uint64 mcause = read_csr(mcause);
+    switch (mcause) {
     case CAUSE_FETCH_ACCESS:
-      handle_instruction_access_fault();
-      break;
+        handle_instruction_access_fault();
+        break;
     case CAUSE_LOAD_ACCESS:
-      handle_load_access_fault();
+        handle_load_access_fault();
     case CAUSE_STORE_ACCESS:
-      handle_store_access_fault();
-      break;
+        handle_store_access_fault();
+        break;
     case CAUSE_ILLEGAL_INSTRUCTION:
-      // TODO (lab1_2): call handle_illegal_instruction to implement illegal instruction
-      // interception, and finish lab1_2.
-      panic( "call handle_illegal_instruction to accomplish illegal instruction interception for lab1_2.\n" );
-
-      break;
+        // TODO (lab1_2): call handle_illegal_instruction to implement illegal
+        // instruction interception, and finish lab1_2.
+        handle_illegal_instruction();
+        break;
     case CAUSE_MISALIGNED_LOAD:
-      handle_misaligned_load();
-      break;
+        handle_misaligned_load();
+        break;
     case CAUSE_MISALIGNED_STORE:
-      handle_misaligned_store();
-      break;
+        handle_misaligned_store();
+        break;
 
     default:
-      sprint("machine trap(): unexpected mscause %p\n", mcause);
-      sprint("            mepc=%p mtval=%p\n", read_csr(mepc), read_csr(mtval));
-      panic( "unexpected exception happened in M-mode.\n" );
-      break;
-  }
+        sprint("machine trap(): unexpected mscause %p\n", mcause);
+        sprint("            mepc=%p mtval=%p\n", read_csr(mepc),
+               read_csr(mtval));
+        panic("unexpected exception happened in M-mode.\n");
+        break;
+    }
 }
